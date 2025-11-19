235763e2bf73e41d615d6f053c285fe4e06d80f1
1rens1 <rein.satyawarman@gmail.com>
2025-11-20 05:46:26 +0700

    #3

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,6 +1,17 @@
+import { renderTemplates } from './templates.js';
+
+renderTemplates();
 lucide.createIcons({
     attrs: {
         width: '1em',
         height: '1em',
     },
 });
+
+const updateAtTop = () => {
+    const atTop = window.scrollY === 0;
+    document.body.dataset.atTop = atTop;
+};
+
+updateAtTop();
+window.addEventListener('scroll', updateAtTop);
--- scripts/templates.js
+++ scripts/templates.js
@@ -2,19 +2,55 @@
 const templates = {
     header: () => {
         const headerElm = document.createElement('header');
-
+        headerElm.innerHTML = `<div class="inner">
+                <div>
+                    <a class="logo" href="/">
+                        <img src="/assets/mascot/icon.svg" alt="Pixo" />
+                        <span>JSP</span>
+                    </a>
+                    <form action="/search.html" class="search">
+                        <button
+                            class="mobile-search-toggle"
+                            onclick="this.nextElementSibling.focus()"
+                            type="button"
+                        >
+                            <i data-lucide="search"></i> Search
+                        </button>
+                        <input
+                            type="search"
+                            name="q"
+                            placeholder="Search for parts"
+                            id="search"
+                            required
+                        />
+                        <button type="reset" onclick="this.previousElementSibling.focus()">
+                            <i data-lucide="x"></i>
+                        </button>
+                        <button type="submit">
+                            <i data-lucide="search"></i>
+                        </button>
+                    </form>
+                </div>
+                <div>
+                    <a href="/about.html">About Us</a>
+                    <a href="/cart.html" class="cart">
+                        <i data-lucide="shopping-cart"></i>
+                    </a>
+                </div>
+            </div>`;
         return headerElm;
     },
 };
 
-export const renderTemplates = () => {
-    Object.keys(templates).forEach((id) => {
-        const elms = document.querySelectorAll(`[data-template-${id}]`);
-        const template = templates[id];
-        elms.forEach((elm) => {
-            const renderedTemplate = template(elm);
-            renderedTemplate.classList.add(id);
-            elm.replaceWith(renderedTemplate);
-        });
+export function renderTemplates() {
+    const nodes = document.querySelectorAll('[data-template]');
+    nodes.forEach((node) => {
+        const id = node.dataset.template;
+        const render = templates[id];
+        if (!render) return;
+
+        const rendered = render(node);
+        rendered.classList.add(id);
+        node.replaceWith(rendered);
     });
-};
+}
