2b8d9d3f54e3fe9b7e3b5ab3030be7234db63375
1rens1 <rein.satyawarman@gmail.com>
2025-11-21 04:49:37 +0700

    #4

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,6 +1,8 @@
-import { renderTemplates } from './templates.js';
+import { categoryTags, products } from './products.js';
+import { renderSimpleIcons, renderTemplates } from './templates.js';
 
 renderTemplates();
+renderSimpleIcons();
 lucide.createIcons({
     attrs: {
         width: '1em',
@@ -15,3 +17,7 @@ const updateAtTop = () => {
 
 updateAtTop();
 window.addEventListener('scroll', updateAtTop);
+
+const debug = document.createElement('pre');
+debug.innerText = JSON.stringify(categoryTags, null, 4);
+// document.body.appendChild(debug);
--- scripts/templates.js
+++ scripts/templates.js
@@ -40,9 +40,60 @@ const templates = {
             </div>`;
         return headerElm;
     },
+    footer: () => {
+        const footerElm = document.createElement('footer');
+        footerElm.innerHTML = `<div class="inner">
+                <div class="footer-main">
+                    <div class="links">
+                        <div>
+                            <a class="logo" href="/">
+                                <img src="/assets/mascot/icon.svg" alt="Pixo" />
+                                <span>JSP</span>
+                            </a>
+                        </div>
+                        <section>
+                            <div>COMPANY</div>
+                            <ul>
+                                <li><a href="/about.html">About Us</a></li>
+                                <li><a href="/tos.html">Terms of Service</a></li>
+                                <li><a href="/pp.html">Privacy Policy</a></li>
+                            </ul>
+                        </section>
+                        <section>
+                            <div>CREDIT</div>
+                            <ul>
+                                <li><a href="javascript:eval('\u0061\u006C\u0065\u0072\u0074\u0028\u0027\u003A\u0050\u0027\u0029')">&#82;&#101;&#105;&#110;</a></li>
+                                <li><a href="javascript:eval('\u0061\u006C\u0065\u0072\u0074\u0028\u0027\u003A\u0050\u0027\u0029')">&#74;&#111;&#97;&#110;&#110;&#97;</a></li>
+                                <li><a href="https://lucide.dev/">Lucide Icons</a></li>
+                                <li><a href="https://simpleicons.org/">Simple Icons</a></li>
+                            </ul>
+                        </section>
+                    </div>
+
+                    <section class="socials">
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0);"><i data-si="instagram"></i></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);"><i data-si="facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);"><i data-si="discord"></i></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);"><i data-si="x"></i></a>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+                <div class="copyright">&copy; <span class="brand">JSP Store</span> 2025</div>
+            </div>`;
+        return footerElm;
+    },
 };
 
-export function renderTemplates() {
+export const renderTemplates = () => {
     const nodes = document.querySelectorAll('[data-template]');
     nodes.forEach((node) => {
         const id = node.dataset.template;
@@ -53,4 +104,52 @@ export function renderTemplates() {
         rendered.classList.add(id);
         node.replaceWith(rendered);
     });
-}
+};
+
+export const renderSimpleIcons = async () => {
+    const iconTemplates = document.querySelectorAll('i[data-si]');
+    if (!iconTemplates.length) return;
+
+    const parser = new DOMParser();
+
+    const slugs = [
+        // Prevent duplicate
+        ...new Set(
+            /** @type {string[]} */
+            Array.from(iconTemplates, (el) => el.dataset.si).filter(Boolean)
+        ),
+    ];
+
+    // Fetch all SVGs in parallel
+    const results = await Promise.all(
+        slugs.map(async (slug) => {
+            const res = await fetch(`https://unpkg.com/simple-icons@v15/icons/${slug}.svg`);
+            return [slug, await res.text()];
+        })
+    );
+
+    // Map for quick lookup
+    const cache = new Map(results);
+
+    // Replace each icon
+    for (const iconTemplate of iconTemplates) {
+        const slug = iconTemplate.dataset.si;
+        if (!slug) continue;
+
+        const svgText = cache.get(slug);
+        if (!svgText) continue;
+
+        const svg = parser.parseFromString(svgText, 'image/svg+xml').querySelector('svg');
+        if (!svg) continue;
+
+        const attrs = {
+            width: '1em',
+            height: '1em',
+            fill: 'currentColor',
+            'stroke-color': 'currentColor',
+        };
+        Object.keys(attrs).forEach((key) => svg.setAttribute(key, attrs[key]));
+
+        iconTemplate.replaceWith(svg);
+    }
+};
